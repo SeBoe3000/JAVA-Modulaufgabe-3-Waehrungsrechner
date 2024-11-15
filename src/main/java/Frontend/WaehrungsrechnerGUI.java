e4b0bfa33a1e4b8f1f6a42f9d4cfcc9155b31fbd
SeBoe3000 <selina.boettger@gmx.de>
2024-11-15 19:47:15 +0100

    Größe und Platzierung Fenster angegeben

--- src/main/java/Frontend/WaehrungsrechnerGUI.java
+++ src/main/java/Frontend/WaehrungsrechnerGUI.java
@@ -1,6 +1,7 @@
 package Frontend;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class WaehrungsrechnerGUI {
     private static final JFrame frame = new JFrame("Waehrungsrechner");
@@ -11,6 +12,12 @@ public class WaehrungsrechnerGUI {
     }
 
     private static void fixxedFrame() {
+        // Größe vom Fenster auf Hälte der Bildschirmgröße in die Mitte setzen
+        Dimension dim = new Dimension(1920, 1080);
+        dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize(dim.width/2, dim.height/2);
+        frame.setLocation(dim.width/4, dim.height/4);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
