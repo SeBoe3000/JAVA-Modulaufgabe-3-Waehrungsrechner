ee9d81747911ee3876a07fe80721d259d3797969
SeBoe3000 <selina.boettger@gmx.de>
2024-11-15 21:02:18 +0100

    Felder hinzugefügt und auf GUI angeordnet

--- src/main/java/Frontend/EingabePanel.java
+++ src/main/java/Frontend/EingabePanel.java
@@ -1,4 +1,47 @@
 package Frontend;
 
-public class EingabePanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class EingabePanel extends JPanel{
+    private JPanel panel = new JPanel();
+    private JLabel label = new JLabel();
+    private JTextField textField = new JTextField();
+    ButtonGroup group = new ButtonGroup();
+
+    public EingabePanel(String labelText){
+        label.setText(labelText);
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        panel.add(label);
+        panel.add(textField);
+        //textField.setMinimumSize(new Dimension(200,50));
+        textField.setPreferredSize(new Dimension(200,50));
+        this.add(panel);
+    }
+    public String getTextfield(){
+        return textField.getText();
+    }
+
+    public String getWaehrung(){
+        return textField.getText();
+    }
+
+    // Methoden zum Schriftfarbe Eingabefeld auf rot setzen und zurück bei einem Fehler:
+    public void setError(){
+        textField.setForeground(Color.RED);
+    }
+
+    public void removeError(){
+        textField.setForeground(Color.BLACK);
+    }
+
+    // Methoden zum möglichen ausgrauen
+    public void setEnabledTrue(){
+        textField.setEnabled(true);
+    }
+
+    public void setEnabledFalse(){
+        textField.setEnabled(false);
+    }
+
 }
--- src/main/java/Frontend/WaehrungsrechnerGUI.java
+++ src/main/java/Frontend/WaehrungsrechnerGUI.java
@@ -6,9 +6,124 @@ import java.awt.*;
 public class WaehrungsrechnerGUI {
     private static final JFrame frame = new JFrame("Waehrungsrechner");
 
+    // Eingabebetrag
+    EingabePanel eingabe = new EingabePanel("Umzurechnender Betrag: ");
 
-    private static void createGUI() {
+    // Eingabewährung
+    JRadioButton ein_euro_rbtn = new JRadioButton("Euro");
+    JRadioButton ein_usd_rbtn = new JRadioButton("USD");
+    JRadioButton ein_bath_rbtn = new JRadioButton("Bath");
+    JRadioButton ein_yen_rbtn = new JRadioButton("Yen");
+    JRadioButton ein_zloty_rbtn = new JRadioButton("Złoty");
+    JLabel eingabewaehrung_lbl = new JLabel("Eingabewaehrung: ");
+    ButtonGroup eingabe_group = new ButtonGroup();
 
+    // Ausgabewährung
+    JRadioButton aus_euro_rbtn = new JRadioButton("Euro");
+    JRadioButton aus_usd_rbtn = new JRadioButton("USD");
+    JRadioButton aus_bath_rbtn = new JRadioButton("Bath");
+    JRadioButton aus_yen_rbtn = new JRadioButton("Yen");
+    JRadioButton aus_zloty_rbtn = new JRadioButton("Złoty");
+    JLabel ausgabewaehrung_lbl = new JLabel("Ausgabewaehrung: ");
+    ButtonGroup ausgabe_group = new ButtonGroup();
+
+    // Wechselkurs
+    EingabePanel wechselkurs = new EingabePanel("Wechselkurs: ");
+    JCheckBox wechselkurs_chb = new JCheckBox("bearbeiten?");
+
+    // Berechnen-Button
+    JButton calc_btn = new JButton("=");
+
+    // Ergebnis
+    EingabePanel ausgabe = new EingabePanel("Ergebnis: ");
+
+    private void createGUI() {
+        JPanel panel = new JPanel();
+        // GridBagLayout
+        GridBagLayout gridbag = new GridBagLayout();
+        GridBagConstraints gbc = new GridBagConstraints();
+        panel.setLayout(gridbag);
+
+        // Feld Eingabe hinzufügen
+        gbc.gridx = 0; // Spalte
+        gbc.gridy = 0; // Zeile
+        gbc.weightx = 0.1;
+        gbc.weighty = 0.1;
+        gbc.fill = GridBagConstraints.CENTER;
+        panel.add(eingabe, gbc);
+
+        // Feld Eingabewährung hinzufügen
+        eingabe_group.add(ein_euro_rbtn);
+        eingabe_group.add(ein_usd_rbtn);
+        eingabe_group.add(ein_bath_rbtn);
+        eingabe_group.add(ein_yen_rbtn);
+        eingabe_group.add(ein_zloty_rbtn);
+
+        JPanel eingabe_radio_panel = new JPanel();
+        eingabe_radio_panel.setLayout(new BoxLayout(eingabe_radio_panel, BoxLayout.X_AXIS));
+        eingabe_radio_panel.add(ein_euro_rbtn);
+        eingabe_radio_panel.add(ein_usd_rbtn);
+        eingabe_radio_panel.add(ein_bath_rbtn);
+        eingabe_radio_panel.add(ein_yen_rbtn);
+        eingabe_radio_panel.add(ein_zloty_rbtn);
+
+        JPanel eingabe_panel = new JPanel();
+        eingabe_panel.setLayout(new BoxLayout(eingabe_panel, BoxLayout.X_AXIS));
+        eingabe_panel.add(eingabewaehrung_lbl);
+        eingabe_panel.add(eingabe_radio_panel);
+
+        gbc.gridy = 1; // Spalte
+        gbc.gridx = 0; // Zeile
+        panel.add(eingabe_panel, gbc);
+
+        // Feld Ausgabewährung hinzufügen
+        ausgabe_group.add(aus_euro_rbtn);
+        ausgabe_group.add(aus_usd_rbtn);
+        ausgabe_group.add(aus_bath_rbtn);
+        ausgabe_group.add(aus_yen_rbtn);
+        ausgabe_group.add(aus_zloty_rbtn);
+
+        JPanel ausgabe_radio_panel = new JPanel();
+        ausgabe_radio_panel.setLayout(new BoxLayout(ausgabe_radio_panel, BoxLayout.X_AXIS));
+        ausgabe_radio_panel.add(aus_euro_rbtn);
+        ausgabe_radio_panel.add(aus_usd_rbtn);
+        ausgabe_radio_panel.add(aus_bath_rbtn);
+        ausgabe_radio_panel.add(aus_yen_rbtn);
+        ausgabe_radio_panel.add(aus_zloty_rbtn);
+
+        JPanel ausgabe_panel = new JPanel();
+        ausgabe_panel.setLayout(new BoxLayout(ausgabe_panel, BoxLayout.X_AXIS));
+        ausgabe_panel.add(ausgabewaehrung_lbl);
+        ausgabe_panel.add(ausgabe_radio_panel);
+
+        gbc.gridy = 2; // Spalte
+        gbc.gridx = 0; // Zeile
+        panel.add(ausgabe_panel, gbc);
+
+        // Feld Wechselkurs hinzufügen
+        JPanel wechselkurs_panel = new JPanel();
+        wechselkurs_panel.setLayout(new BoxLayout(wechselkurs_panel, BoxLayout.X_AXIS));
+        wechselkurs_panel.add(wechselkurs);
+        wechselkurs_panel.add(wechselkurs_chb);
+
+        gbc.gridy = 3; // Spalte
+        gbc.gridx = 0; // Zeile
+        panel.add(wechselkurs_panel, gbc);
+
+
+        // Feld Berechnen hinzufügen
+        gbc.gridy = 4; // Spalte
+        gbc.gridx = 0; // Zeile
+        calc_btn.setPreferredSize(new Dimension(400,50));
+        panel.add(calc_btn, gbc);
+
+        // Feld Ausgabe Ergebnis hinzufügen
+        gbc.gridy = 5; // Spalte
+        gbc.gridx = 0; // Zeile
+        panel.add(ausgabe, gbc);
+
+        // Panel dem Frame hinzufügen
+        frame.add(panel);
     }
 
     private static void fixxedFrame() {
