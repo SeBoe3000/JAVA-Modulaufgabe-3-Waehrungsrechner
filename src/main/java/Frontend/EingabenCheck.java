cb451b33c7c6263f6f40483ff23648a799e1ca30
SeBoe3000 <selina.boettger@gmx.de>
2024-11-17 12:32:42 +0100

    Prüfungen zusammengefasst

--- src/main/java/Frontend/EingabenCheck.java
+++ src/main/java/Frontend/EingabenCheck.java
@@ -3,20 +3,17 @@ package Frontend;
 public class EingabenCheck {
     public static boolean isValidIntString(String eingabe){
         boolean isValid = true;
+        Integer anzahlPunkte = 0;
+        Integer anzahlZahl = 0;
 
         // Nur Zahlen und Punkte sind zulässige Zeichen
+        // Nur ein Punkt darf vorhanden sein.
+        // Mindestens eine Zahl zwischen 1 und 9 muss angegeben sein. (Dadurch Überprüfung auf nicht leerer Wert)
         for(int i = 0; i < eingabe.length(); i++){
             if(!((eingabe.charAt(i) >= '0' && eingabe.charAt(i) <= '9')||eingabe.charAt(i) == '.')){
                 isValid = false;
                 break;
             }
-        }
-
-        // Nur ein Punkt darf vorhanden sein.
-        // Es muss mindestens eine Zahl zwischen 1 und 9 angegeben sein. (Dadurch auch Überprüfung auf nicht leerer Wert)
-        Integer anzahlPunkte = 0;
-        Integer anzahlZahl = 0;
-        for(int i = 0; i < eingabe.length(); i++){
             if((eingabe.charAt(i) >= '1' && eingabe.charAt(i) <= '9')){
                 anzahlZahl ++;
             }
@@ -24,6 +21,7 @@ public class EingabenCheck {
                 anzahlPunkte ++;
             }
         }
+
         //System.out.println("anzahlPunkte " + anzahlPunkte + " anzahlZahl " + anzahlZahl);
         if (anzahlPunkte > 1 || anzahlZahl == 0){
             isValid = false;
