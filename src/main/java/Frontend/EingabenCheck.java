318258dc78af51caebfb1c3e4d01d241f93b16ce
SeBoe3000 <selina.boettger@gmx.de>
2024-11-17 12:23:28 +0100

    Punkte als gültige Angabe hinzugefügt. Es darf nur ein Punkt angegeben sein. Ebenfalls muss eine Zahl zwischen 1 und 9 eingegeben sein. Dadurch Prüfung auf gleich leer oder Null hinfällig.

--- src/main/java/Frontend/EingabenCheck.java
+++ src/main/java/Frontend/EingabenCheck.java
@@ -3,15 +3,32 @@ package Frontend;
 public class EingabenCheck {
     public static boolean isValidIntString(String eingabe){
         boolean isValid = true;
+
+        // Nur Zahlen und Punkte sind zulässige Zeichen
         for(int i = 0; i < eingabe.length(); i++){
-            if(!(eingabe.charAt(i) >= '0' && eingabe.charAt(i) <= '9')){
+            if(!((eingabe.charAt(i) >= '0' && eingabe.charAt(i) <= '9')||eingabe.charAt(i) == '.')){
                 isValid = false;
                 break;
             }
         }
-        if(eingabe.equals("")||eingabe.equals("0")){
-            return false;
+
+        // Nur ein Punkt darf vorhanden sein.
+        // Es muss mindestens eine Zahl zwischen 1 und 9 angegeben sein. (Dadurch auch Überprüfung auf nicht leerer Wert)
+        Integer anzahlPunkte = 0;
+        Integer anzahlZahl = 0;
+        for(int i = 0; i < eingabe.length(); i++){
+            if((eingabe.charAt(i) >= '1' && eingabe.charAt(i) <= '9')){
+                anzahlZahl ++;
+            }
+            if(eingabe.charAt(i) == '.'){
+                anzahlPunkte ++;
+            }
         }
+        //System.out.println("anzahlPunkte " + anzahlPunkte + " anzahlZahl " + anzahlZahl);
+        if (anzahlPunkte > 1 || anzahlZahl == 0){
+            isValid = false;
+        }
+
         return isValid;
     }
 }
